e1b04a3cfa44bf362a9d4e26f28be4bfb55f8536
andrei <mr.vakar2016@gmail.com>
2022-01-10 03:59:53 +0300

    Add sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 require "active_support/core_ext/integer/time"
-
+require "pry"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -73,7 +73,19 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.smtp_settings = {
+    :port => '587',
+    :authentication => :plain,
+    :domain => 'localhost:3000',
+    :address => 'smtp.sendgrid.net',
+    :user_name => Rails.application.credentials.dig(:sendgrid, :user_name),
+    :password => Rails.application.credentials.dig(:sendgrid, :password),
+    :enable_starttls_auto => true
+  }
 end
