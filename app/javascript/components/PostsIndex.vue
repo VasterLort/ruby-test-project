ad4072deda171644123986dd10c9207bd1dcd088
andrei <mr.vakar2016@gmail.com>
2022-02-17 00:02:52 +0300

    add component for adding posts

--- app/javascript/components/PostsIndex.vue
+++ app/javascript/components/PostsIndex.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-10">
-        <h1>Posts</h1>
-        <hr><br><br>
-        <button type="button" class="btn btn-success btn-sm">Add Post</button>
-        <br><br>
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Description</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(post, index) in posts" :key="index">
-              <td>{{ post.title }}</td>
-              <td>{{ post.description }}</td>
-              <td>
-                <button type="button" class="btn btn-warning btn-sm">Update</button>
-                <button type="button" class="btn btn-danger btn-sm">Delete</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
+  <v-container class="mt-3">
+    <post-create-form
+      @response='getPosts'
+    ></post-create-form>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Id
+            </th>
+            <th class="text-left">
+              Title
+            </th>
+            <th class="text-left">
+              Description
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="post in posts"
+            :key="post.id"
+          >
+            <td>{{ post.id }}</td>
+            <td>{{ post.title }}</td>
+            <td>{{ post.description }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
 </template>
 
 <script>
@@ -39,17 +42,19 @@ export default {
       posts: [],
     };
   },
+
   methods: {
     getPosts() {
       axios.get('/posts.json')
-        .then((res) => {
-          this.posts = res.data;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      .then((res) => {
+        this.posts = res.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
     },
   },
+
   created() {
     this.getPosts();
   }
--- app/javascript/packs/components.js
+++ app/javascript/packs/components.js
@@ -1,4 +1,6 @@
 import Vue from "vue/dist/vue.esm"
 import PostsIndex from "../components/PostsIndex.vue"
+import PostCreateForm from '../components/PostCreateForm.vue'
 
 Vue.component('post-index', PostsIndex)
+Vue.component('post-create-form', PostCreateForm)
